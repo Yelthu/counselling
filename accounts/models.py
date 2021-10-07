21d7319a045ba560585f9085a19e511ef01195ee
Ye Lin Thu <yelinthu@Yes-MacBook-Pro.local>
2021-10-07 21:20:28 +0630

    update acc model

--- accounts/models.py
+++ accounts/models.py
@@ -3,12 +3,30 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 
 # Create your models here.
 class UserAccountManager(BaseUserManager):
-    def create_user(self, email, name, password=None):
+
+    def create_superuser(self, email, name,  password, **other_fields):
+
+        other_fields.setdefault('is_staff',True)
+        other_fields.setdefault('is_superuser',True)
+        other_fields.setdefault('is_active',True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_staff=True'
+            )
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_staff=True'
+            )    
+
+        return self.create_user(email, name, password, **other_fields)
+
+    def create_user(self, email, name, password=None, **other_fields):
         if not email:
             raise ValueError('User must have an email address')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
+        user = self.model(email=email, name=name, **other_fields)
 
         user.set_password(password)
         user.save()
